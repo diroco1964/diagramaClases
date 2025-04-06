ac2400d72ceb989bdf3bf573a2a33bb450d77a33
juanisanchez2020 <juani.scz95@gmail.com>
2025-04-06 10:26:05 -0300

    Gestion Empleados

---
+++ gestionEmpleado/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        SistemaGestion sistema = new SistemaGestion();
+
+        Departamento  departamentoIT = new Departamento("Tecnología");
+        Puesto desarrollador =new Puesto("Desarrollador", 120000);
+     
+
+        Empleado empleado1 = new Empleado("Laura");
+       
+
+        sistema.AgregarEmpleado(empleado1);
+       
+
+        sistema.AsignarDepartamento(empleado1,departamentoIT);
+      
+
+        sistema.AsignarPuesto(empleado1,desarrollador);
+        
+
+        sistema.MostrarSalarios();
+    }
+}
+public class Empleado{
+    public string nombre;
+    public Puesto puesto;
+
+    public Departamento departameto;
+    public  Empleado(string nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public double CalcularSalario()
+    {
+        return puesto != null ? puesto.salarioBasico : 0;
+    }
+}
+
+public class Puesto{
+    public string nombre;
+    public double salarioBasico;
+
+    public Puesto(string nombre, double salarioBasico)
+    {
+        this.nombre = nombre;
+        this.salarioBasico = salarioBasico;
+    }
+
+}
+
+public class Departamento
+{
+    public string nombre;
+
+    public Departamento(string nombre)
+    {
+        this.nombre = nombre;
+    }
+}
+
+public class SistemaGestion
+{
+    public  List<Empleado> empleados = new List<Empleado>();
+
+    public void AgregarEmpleado(Empleado empleado)
+    {
+        empleados.Add(empleado);
+    }
+
+    public void AsignarPuesto(Empleado empleado, Puesto puesto)
+    {
+        empleado.puesto = puesto;
+    }
+
+    public void AsignarDepartamento(Empleado empleado, Departamento departamento)
+    {
+        empleado.departameto= departamento;
+    }
+
+    public void MostrarSalarios()
+    {
+        foreach (Empleado empleado in empleados)
+        {
+            Console.WriteLine($"{empleado.nombre} - {empleado.puesto?.nombre} - ${empleado.CalcularSalario()}");
+        }
+    }
+}
